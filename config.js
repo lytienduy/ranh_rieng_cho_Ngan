a9f5a9175e4934ee2671c49939317d1754b16542
lytienduy <133567310+lytienduy@users.noreply.github.com>
2023-12-14 14:24:58 +0700

    Add files via upload
    create card

---
+++ config.js
@@ -0,0 +1,10 @@
+const nameGirl = 'Em💘';
+const giftUrl = 'https://www.facebook.com/lytienduy2004/';
+const eventName = 'Merry Christmas';
+const titleCard = 'Tặng Ngân <3';
+const contentCard = 'Chúc honey của anh giáng sinh tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với em trong hôm nay và cả những ngày sau';
+
+
+const giftImage = 'hot-girl.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
